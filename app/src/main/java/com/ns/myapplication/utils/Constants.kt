c0fcf5e90a9e276d30d1956729eb6958cf785be8
Enes <arisoyeness@gmail.com>
2024-04-12 14:32:08 +0300

    Constants

---
+++ app/src/main/java/com/ns/myapplication/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.ns.myapplication.utils
+
+object Constants {
+
+    const val BASE_URL = "https://espresso-food-delivery-backend-cc3e106e2d34.herokuapp.com"
+    const val USER_EMAIL = "enes@gmail.com"
+    const val USER_PASS = "Enes123?"
+}
